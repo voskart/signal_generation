814de9fbd07ab438f431e667adffd41af9fe0a58
Artemij Voskobojnikov <voskobojnikov.artemij@gmail.com>
2024-01-25 09:34:29 -0700

    initial commit

---
+++ src/db_connection.py
@@ -0,0 +1,26 @@
+import pymongo
+import os
+from dotenv import load_dotenv
+
+class MongoConnection():
+    URI = 'mongodb+srv://cluster0.2zoytm3.mongodb.net'
+    client = None
+
+    def __init__(self) -> None:
+        load_dotenv()
+        self.client = pymongo.MongoClient(MongoConnection.URI, username=os.environ.get('USER_NAME'), password=os.environ.get('PASSWORD'))
+
+    def get_database(self):
+        try:
+            return self.client.get_database('velodata')
+        except:
+            raise(NameError('Not Found'))
+
+
+def main():
+    mongoConnection = MongoConnection()
+    db = mongoConnection.client['velodata']
+    print(f'Collections: {db.list_collection_names()}')
+
+if __name__ == '__main__':
+    main()
---
+++ src/signal_test.py
@@ -0,0 +1,33 @@
+from db_connection import MongoConnection
+from pymongo.errors import ConnectionFailure, CollectionInvalid
+import datetime as dt
+import pandas as pd
+
+class Signal():
+    
+    db = None
+
+    def __init__(self) -> None:
+        try:
+            mc = MongoConnection()
+            self.db = mc.client['velodata']
+        except ConnectionFailure as e:
+            raise e
+
+    def get_futures_data(self, product='ETHUSDT', exchange='binance-futures', start_date=dt.datetime(2024, 1, 1), end_date=dt.datetime.now()):
+        try:
+            return self.db.futures.find({
+                'product': product,
+                'exchange': exchange,
+            })
+        except CollectionInvalid as e:
+            raise e
+    
+def main():
+    sig = Signal()
+    futures_data = list(sig.get_futures_data())
+    df = pd.DataFrame(futures_data)
+    df.to_csv('../data/jan_data.csv')
+
+if __name__ == '__main__':
+    main()
